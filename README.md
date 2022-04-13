a45f302ae477ce5c5a0f6bdbc38cc9168ed96bf6
Carlos Silva Jorquera <csilvajo@hotmail.com>
2022-04-13 18:47:09 -0400

    Primer commit, creacion del repositorio

---
+++ README.md
@@ -0,0 +1,9 @@
+# Landing Page
+Página web tipo LandingPage, con diseño responsivo.
+
+## Link para acceder a la página
+ https://csilvajo.github.io/PortafolioBootstrap/
+
+## Tecnologías
+
+HTML5, CSS3, Bootstrap4
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//habilitacion de ToolTip de Bootstrap
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
